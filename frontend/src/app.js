abd365ee0df67e345d06fbdde59524ad13f4369c
Andreas Wittig <andreas@widdix.de>
2019-02-28 15:00:45 +0100

    updating client config

--- frontend/src/app.js
+++ frontend/src/app.js
@@ -5,8 +5,8 @@ import { InMemoryCache } from 'apollo-cache-inmemory';
 import gql from "graphql-tag";
 import Vue from 'vue'
 
-const APPSYNC_URI = 'https://ycbelti755gonbamz4blsdriuu.appsync-api.eu-west-1.amazonaws.com/graphql';
-const APPSYNC_APIKEY = 'da2-p2mz2252svespmtyr3bwcmbw2e';
+const APPSYNC_URI = 'https://v233izxeofbg7mpx6pslew2wwm.appsync-api.eu-west-1.amazonaws.com/graphql';
+const APPSYNC_APIKEY = 'da2-7scjjscmlzcqtgkjm4kfv5czaa';
 
 
 const httpLink = createHttpLink({
